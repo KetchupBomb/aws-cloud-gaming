a209bb843c24ed236e2f011ec27e301e6098fbc5
Nicolas Lang <git@nicolas-lang.de>
2023-11-30 19:24:39 +0100

    fixed output parsing; fixed accountname; added call to copy pw to clipboard

--- connect_with_rdp.ps1
+++ connect_with_rdp.ps1
@@ -1,11 +1,9 @@
 #!/usr/bin/env pwsh
-
-$ip = (terraform output instance_ip) | Out-String
-$user = "Administrator"
-
+[string] $ip = ((terraform output instance_ip) | Out-String).Trim().Trim('"')
+$user = "$ip\Administrator"
 @"
 full address:s:$ip
 username:s:$user
 "@ | Out-File -Encoding ASCII  -FilePath "aws-cloud-gaming.rdp"
 Invoke-Item "aws-cloud-gaming.rdp"
-
+.\copy_administrator_password.ps1
